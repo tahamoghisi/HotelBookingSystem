67fa1e73f460898d388cb4b605ef7525cb65656a
TahaMoghisi <tahamogheisi@gmail.com>
2026-08-25 15:05:10 +0330

    Create AuthController

---
+++ HotelBooking.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using HotelBooking.Application.DTOs.Auth;
+using HotelBooking.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+              _authService = authService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var user = await _authService.LoginAsync(loginRequest);
+            return Ok(user);
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+        {
+            var user = await _authService.RegisterAsync(registerRequest);
+            return Ok(user);
+        }
+    }
+}
